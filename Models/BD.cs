4774c2888eb1500a47772437b269b4f4ca69370d
Pekeitu <106318905+Pekeitu@users.noreply.github.com>
2022-06-23 10:27:49 -0300

    BD.cs Update
    Complete la consigna

--- Models/BD.cs
+++ Models/BD.cs
@@ -13,34 +13,62 @@ namespace TP06_Qatar.Services
         {
             using(SqlConnection db = new SqlConnection(_connectionString))
             {
-                string sql = "INSERT INTO Jugadores (IdEquipo, Nombre, FechaNacimiento, Foto, EquipoActual) VALUES(@IdEquipo, @Nombre, @FechaNacimiento, @Foto, @EquipoActual)";
+                string sqlQuery = "INSERT INTO Jugadores (IdEquipo, Nombre, FechaNacimiento, Foto, EquipoActual) VALUES(@IdEquipo, @Nombre, @FechaNacimiento, @Foto, @EquipoActual)";
                 int affectedRows = db.Execute<Jugador>(sqlQuery , new {idEquipo = jug.IdEquipo, Nombre = jug.Nombre, FechaNacimiento = Jug.FechaNacimiento, Foto = Jug.Foto, EquipoActual = Jug.EquipoActual});
             }
         }
 
         public static void EliminarJugador(int idJugador)
         {
-
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "DELETE FROM Jugadores WHERE IdJugador = @pidJugador";
+                int affectedRows = db.Execute<Jugador>(sqlQuery , new {pidJugador = idJugador});
+            } 
         }
 
         public static Equipo VerInfoEquipo(int idEquipo)
         {
-
+            Equipo MiEquipo = null;
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "SELECT * FROM Equipos WHERE IdEquipo = @pidEquipo";
+                MiEquipo = db.QueryFirstOrDefault<Equipo>(sqlQuery, new{pidEquipo = idEquipo});
+            }
+            return MiEquipo;
         }
 
         public static Jugador VerInfoJugador(int idJugador)
         {
-
+            Jugador MiJugador = null;
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "SELECT * FROM Jugadores WHERE IdJugador= @pidJugador";
+                MiJugador = db.QueryFirstOrDefault<Jugador>(sqlQuery, new{pidJugador = idJugador});
+            }
+            return MiJugador;
         }
 
         public static List<Equipo> ListarEquipos()
         {
-
+            List<Equipo> _MiEquipo = null;
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "SELECT * FROM Jugadores";
+                _MiEquipo = db.Query<Equipos>(sqlQuery, new{pidEquipo = idEquipo}).ToList();
+            }
+            return _MiEquipo;
         }
 
         public static List<Jugador> ListarJugadores(int idEquipo)
         {
-
+            List<Jugador> _MiJugador = null;
+            using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sqlQuery = "SELECT * FROM Jugadores WHERE idEquipo = @pidEquipo";
+                _MiJugador = db.Query<Jugador>(sqlQuery).ToList();
+            }
+            return _MiJugador;
         }
     } 
 }
