7c29db8341a02aef4219a900dbf7dd9db22acebc
Pekeitu <106318905+Pekeitu@users.noreply.github.com>
2022-06-23 11:45:59 -0300

    Update Controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,7 +20,40 @@ namespace TP06_Qatar.Controllers
 
         public IActionResult Index()
         {
+            List<Equipos> ListTeams = new List<Equipos>();
+            ListTeams = BD.ListarEquipos();
+
+            ViewBag.ListaDeEquipos = ListTeams;
             return View();
+        }
+
+        public IActionResult VerDetalleEquipo(int IdEquipo){
+
+            ViewBag.DatosEquipo = BD.VerInfoEquipo(IdEquipo);
+            ViewBag.ListaJugadores = BD.ListarJugadores(IdEquipo);
+
+            return View("DetalleEquipo");
+        }
+
+        public IActionResult VerDetalleJugador(int IdJugador){  
+            ViewBag.DatosJugador = BD.VerDetalleJugador(IdJugador);
+
+            return View("DetalleJugador");
+        }
+
+        public IActionResult AgregarJugador(int IdEquipo){
+            ViewBag.IdEquipo = IdEquipo;
+            return View("PlayerForm")
+        }
+        public [HttpPost] IActionResult GuardarJugador(Jugador){
+            BD.AgregarJugador(Jugador);
+
+            return RedirectToAction("VerDetalleEquipo", new {int IdEquipo});
+        }
+
+        public IActionResult EliminarJugador(int IdJugador, int IdEquipo){
+            BD.EliminarJugador(idJugador);
+            return RedirectToAction("VerDetalleEquipo", new {int IdEquipo});
         }
 
         public IActionResult Privacy()
