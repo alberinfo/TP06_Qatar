d0936a4960a8d28c1f812acc0accf64a625f11bf
alberinfo <43301417+alberinfo@users.noreply.github.com>
2022-07-14 16:16:45 -0300

    Fix most style related problems

--- Models/BD.cs
+++ Models/BD.cs
@@ -8,7 +8,7 @@ namespace TP06_Qatar.Models
 {
     public static class BD
     {
-        private static string _connectionString = @"Server=A-CIDI-113;DataBase=Qatar;Trusted_Connection=True";
+        private static string _connectionString = @"Server=A-LUM-2;DataBase=Qatar;Trusted_Connection=True";
 
         public static void AgregarJugador(Jugador jug)
         {
